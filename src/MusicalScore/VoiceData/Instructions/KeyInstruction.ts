dd95a1476ea367c72783572fac96c9a12a755f0f
Stefan Werner <stefan.werner@insiberia.net>
2025-12-10 15:10:34 +0100

    feat: update localization strings; add coverage exercise type and improve descriptions

--- src/MusicalScore/VoiceData/Instructions/KeyInstruction.ts
+++ src/MusicalScore/VoiceData/Instructions/KeyInstruction.ts
@@ -27,7 +27,7 @@ export class KeyInstruction extends AbstractNotationInstruction {
     public isTransposedBy: number = 0;
 
     public static copy(keyInstruction: KeyInstruction): KeyInstruction {
-        const newKeyInstruction: KeyInstruction = new KeyInstruction(keyInstruction.parent, keyInstruction.Key, keyInstruction.Mode);
+        const newKeyInstruction: KeyInstruction = new KeyInstruction(keyInstruction?.parent, keyInstruction.Key, keyInstruction.Mode);
         // note that newKeyInstruction.keyTypeOriginal is set incorrectly in the constructor, but we fix that here:
         newKeyInstruction.keyTypeOriginal = keyInstruction.keyTypeOriginal;
         return newKeyInstruction;
